1ca602f21ec42b5f1dd6c286d8bbf6b6300f1121
AynurMv <aynurmotigulov@gmail.com>
2022-09-15 10:03:07 +0300

    initialization is done!

---
+++ src/components/Layout.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
+
+export default function Layout({ initState }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/stylesheets/application.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossOrigin="anonymous" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `window.initState=${JSON.stringify(initState)}`,
+          }}
+        />
+        <script defer src="/app.js" />
+        <script defer src="/vendor.js" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div id="root">
+          <StaticRouter location={initState.path}>
+            <App {...initState} />
+          </StaticRouter>
+        </div>
+      </body>
+    </html>
+  );
+}
